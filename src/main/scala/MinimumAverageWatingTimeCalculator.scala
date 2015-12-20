359d30645e9e14838141f8b787def38542ce91f2
michaelgruczel <michael.gruczel@gmx.de>
2015-12-20 21:18:49 +0100

    check some easy preconditions

--- src/main/scala/MinimumAverageWatingTimeCalculator.scala
+++ src/main/scala/MinimumAverageWatingTimeCalculator.scala
@@ -1,7 +1,22 @@
 object MinimumAverageWatingTimeCalculator {
 
   def calculate(numberOfCustomers:Int = 0, times:List[(Int, Int)] = List()) : Int = {
+
+    checkConsistencyOfInputData(numberOfCustomers, times)
+
     ???
+  }
+
+  /*
+   * number of customer between 1 and 100000
+   * and for every cutomer a pair of ordering time and working time is given
+   */
+  def checkConsistencyOfInputData(numberOfCustomers: Int, times: List[(Int, Int)]): Unit = {
+    if (numberOfCustomers == 0
+      || times.length == 0
+      || numberOfCustomers != times.length
+      || numberOfCustomers > 100000
+    ) throw new scala.IllegalArgumentException
   }
 
 }
